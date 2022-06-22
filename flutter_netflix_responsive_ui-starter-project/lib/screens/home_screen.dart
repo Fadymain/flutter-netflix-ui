b119918d38157b9ee6b72bf4159be53b4bcae5c8
Adetola <adetolafadairo@gmail.com>
2022-06-21 20:27:15 -0700

    creating screens folder for applications different screens

---
+++ flutter_netflix_responsive_ui-starter-project/lib/screens/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+
+  @override
+  _HomeScrenState createState() => _HomeScrenState();
+}
+
+class _HomeScrenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
